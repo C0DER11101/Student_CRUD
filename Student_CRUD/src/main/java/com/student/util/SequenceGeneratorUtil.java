aff7178848dd6260efdce6974106dcdfa9e3261e
shashi-sahoo <sahooshashikanta2003@gmail.com>
2026-09-03 11:33:13 +0530

    Modified AltKey

--- Student_CRUD/src/main/java/com/student/util/SequenceGeneratorUtil.java
+++ Student_CRUD/src/main/java/com/student/util/SequenceGeneratorUtil.java
@@ -1,4 +1,12 @@
 package com.student.util;
 
+import java.math.BigInteger;
+
 public class SequenceGeneratorUtil {
+
+    //STU<NUMBER> -> Random Id
+    public static String generateAltKey() {
+        return "STU" + (100 + Math.random() * (Integer.MAX_VALUE));
+    }
+
 }
