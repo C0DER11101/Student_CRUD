5b0db1331a47bf5e4f6bd97fc5b4a61d6b09499e
shashi-sahoo <sahooshashikanta2003@gmail.com>
2026-09-03 18:23:54 +0530

    DBConnector

---
+++ Student_CRUD/src/main/java/com/student/dbconnector/DBConnector.java
@@ -0,0 +1,9 @@
+package com.student.dbconnector;
+
+public abstract class DBConnector {
+
+    private static final String URL = "mysql://avnadmin:AVNS_k6sa7LzNh_FgjXI41MD@library-config4-library4-config.k.aivencloud.com:25455/defaultdb?ssl-mode=REQUIRED";
+    private static final String USERNAME = "avnadmin";
+    private static final String PASSWORD = "AVNS_k6sa7LzNh_FgjXI41MD";
+
+}
