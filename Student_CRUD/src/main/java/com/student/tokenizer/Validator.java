3d870d2d0f9e1c6b93714640da8c6ec4df41ba62
C0DER11101 <javapr0gramm3r@yahoo.com>
2026-09-04 20:52:33 +0530

    Validator

--- Student_CRUD/src/main/java/com/student/tokenizer/Validator.java
+++ Student_CRUD/src/main/java/com/student/tokenizer/Validator.java
@@ -1,5 +1,12 @@
 package com.student.tokenizer;
 
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+import java.util.Map;
+
+// Syntax validation
+@Component
 public class Validator implements Tokenizer {
     @Override
     public void parse(String command) {
