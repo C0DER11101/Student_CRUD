c9c213f973c1deb8e51f29ae5118b47a87f4496e
C0DER11101 <javapr0gramm3r@yahoo.com>
2026-09-04 20:52:18 +0530

    added command registry

--- Student_CRUD/src/main/java/com/student/command/CommandRegistry.java
+++ Student_CRUD/src/main/java/com/student/command/CommandRegistry.java
@@ -4,4 +4,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 public abstract class CommandRegistry {
+
+    public static final Map<String, String> COMMANDS = Map.of("help", "help",
+            "add", "add student",
+            "findAll", "find all",
+            "find", "find",
+            "fetch", "fetch field in");
 }
